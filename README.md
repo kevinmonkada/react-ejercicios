3802495e4327e0fc19b0264cf9c8482b2e673808
Kevin Moncada <kevinmonkada@gmail.com>
2023-11-26 17:08:59 -0500

    Ejercicio de Select anidados

---
+++ README.md
@@ -0,0 +1,3 @@
+#react-Excercises
+
+This is a repo to show some of the excercises created through the React course provided by jonmircha.
--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import CrudApp from "./components/CrudApp";
 import CrudApi from "./components/CrudApi";
 import SongSearch from "./components/SongSearch";
+import SelectsAnidados from "./components/SelectsAnidados";
 
 function App() {
   return (
     <>
       <h1>Ejercicios con React</h1>
+      <hr />
+      <SelectsAnidados />
       <hr />
       <SongSearch />
       <hr />

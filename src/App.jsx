f0b5bb26284b8e983c6188836b69f01a6d5a82a7
Kevin Moncada <kevinmonkada@gmail.com>
2023-11-27 18:29:04 -0500

    Form Validations and minor fix to the loader

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import CrudApp from "./components/CrudApp";
 import CrudApi from "./components/CrudApi";
 import SongSearch from "./components/SongSearch";
 import SelectsAnidados from "./components/SelectsAnidados";
+import ContactForm from "./components/ContactForm";
 
 function App() {
   return (
     <>
       <h1>Ejercicios con React</h1>
+      <hr />
+      <ContactForm />
       <hr />
       <SelectsAnidados />
       <hr />
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import "./Loader.css";
 
 const Loader = () => {
-  return (
-    <div className="lds-circle">
-      <div></div>
-    </div>
-  );
+  return <div className="lds-dual-ring"></div>;
 };
 
 export default Loader;

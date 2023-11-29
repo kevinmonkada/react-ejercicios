0d01c8f64c4ec03eda1becaf4510683546306ba3
Kevin Moncada <kevinmonkada@gmail.com>
2023-11-29 15:05:34 -0500

    Modals in React

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,14 @@ import CrudApi from "./components/CrudApi";
 import SongSearch from "./components/SongSearch";
 import SelectsAnidados from "./components/SelectsAnidados";
 import ContactForm from "./components/ContactForm";
+import Modals from "./components/Modals";
 
 function App() {
   return (
     <>
       <h1>Ejercicios con React</h1>
+      <hr />
+      <Modals />
       <hr />
       <ContactForm />
       <hr />
